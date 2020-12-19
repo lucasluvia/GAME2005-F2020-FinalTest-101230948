9f11af5785107981ca3ea43e3add030f1b811e0c
lucasdunster <60508486+lucasluvia@users.noreply.github.com>
2020-12-19 16:52:39 -0500

    Initial Commit

---
+++ Assets/_Scripts/MainKeyPress.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainKeyPress : MonoBehaviour
+{
+
+    public string newStartScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isKeyDown = Input.GetKeyDown(KeyCode.L);
+        if (isKeyDown)
+        {
+            SceneManager.LoadScene(newStartScene);
+        }
+    }
+
+    
+}
